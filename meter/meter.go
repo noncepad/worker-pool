2c5973f1c2635692212cbd6d8fa625370ad50f4b
Joel De Jesus <joel@noncepad.com>
2024-05-07 01:03:12 +0900

    updating capacity to receive real time updates

--- meter/meter.go
+++ meter/meter.go
@@ -14,13 +14,27 @@ type Hook struct {
 	busyWorkers  int
 	totalWorkers int
 	m            *sync.Mutex
+	subIndex     int
+	subM         map[int]*hookSub
+}
+
+type hookSub struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	i      int
+	outC   chan<- float32
 }
 
 func (h *Hook) calculateCapacity() float32 {
 	h.m.Lock()
+	defer h.m.Unlock()
+	return h.unsafeCalculateCapacity()
+}
+
+func (h *Hook) unsafeCalculateCapacity() float32 {
+
 	a := h.busyWorkers
 	b := h.totalWorkers
-	h.m.Unlock()
 
 	capacity := float32(a) / float32(b)
 
@@ -44,12 +58,34 @@ func (h *Hook) OnStatus(req *pbt.Empty, stream pbt.Capacity_OnStatusServer) erro
 	ticker := time.NewTicker(30 * time.Second)
 	defer ticker.Stop()
 
+	h.m.Lock()
+	x := new(hookSub)
+	x.i = h.subIndex
+	h.subIndex++
+	changeC := make(chan float32, 100)
+	x.outC = changeC
+	x.ctx = ctx
+	x.cancel = cancel
+	h.subM[x.i] = x
+	h.m.Unlock()
+
+	var err error
 	// Loop to send a response every time the ticker ticks
+out:
 	for {
 		select {
 		case <-ctx.Done():
 			// stream closed
-			return nil
+			break out
+		case capacity := <-changeC:
+			if 1 < capacity {
+				capacity = 1
+			} else if capacity < 0 {
+				capacity = 0
+			}
+			if err = stream.Send(&pbt.CapacityStatus{UtilizationRatio: capacity}); err != nil {
+				break out
+			}
 		case <-ticker.C:
 			// Send a response
 			capacity := h.calculateCapacity()
@@ -58,11 +94,15 @@ func (h *Hook) OnStatus(req *pbt.Empty, stream pbt.Capacity_OnStatusServer) erro
 			} else if capacity < 0 {
 				capacity = 0
 			}
-			if err := stream.Send(&pbt.CapacityStatus{UtilizationRatio: capacity}); err != nil {
-				return err
+			if err = stream.Send(&pbt.CapacityStatus{UtilizationRatio: capacity}); err != nil {
+				break out
 			}
 		}
 	}
+	h.m.Lock()
+	delete(h.subM, x.i)
+	h.m.Unlock()
+	return err
 }
 func Create(s *grpc.Server) *Hook {
 
@@ -70,6 +110,8 @@ func Create(s *grpc.Server) *Hook {
 		busyWorkers:  0,
 		totalWorkers: 0,
 		m:            &sync.Mutex{},
+		subIndex:     0,
+		subM:         make(map[int]*hookSub),
 	}
 	pbt.RegisterCapacityServer(s, hook)
 	return hook
@@ -79,22 +121,38 @@ func (h *Hook) JobStart() {
 	h.m.Lock()
 	defer h.m.Unlock()
 	h.busyWorkers++
+	h.unsafeCapacityUpdate()
 }
 
 func (h *Hook) JobFinish() {
 	h.m.Lock()
 	defer h.m.Unlock()
 	h.busyWorkers--
+	h.unsafeCapacityUpdate()
 }
 
 func (h *Hook) WorkerAdd() {
 	h.m.Lock()
 	defer h.m.Unlock()
 	h.totalWorkers++
+	h.unsafeCapacityUpdate()
 }
 
 func (h *Hook) WorkerRemove() {
 	h.m.Lock()
 	defer h.m.Unlock()
 	h.totalWorkers--
+	h.unsafeCapacityUpdate()
+}
+
+func (h *Hook) unsafeCapacityUpdate() {
+	capacity := h.unsafeCalculateCapacity()
+	for _, v := range h.subM {
+		select {
+		case v.outC <- capacity:
+		default:
+			v.cancel()
+			delete(h.subM, v.i)
+		}
+	}
 }
